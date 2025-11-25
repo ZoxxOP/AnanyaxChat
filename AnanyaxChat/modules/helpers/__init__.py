347b792a1e8d3c3c560f755bd244936efee004ae
𝐀𝐤𝐚𝐬𝐡 𝐃𝐚𝐤𝐬𝐡𝐰𝐚𝐧𝐬𝐡𝐢 : ZoxxOP <akp954834@gmail.com>
2025-11-25 22:38:40 +0530

    Update and rename __init__.py to __init__.py

--- AnanyaxChat/modules/helpers/__init__.py
+++ AnanyaxChat/modules/helpers/__init__.py
@@ -3,11 +3,11 @@ from typing import Callable
 from pyrogram.enums import ChatMemberStatus
 from pyrogram.types import Message
 
-from ShrutiCHATBOT import OWNER, ShrutiCHATBOT
+from AnanyaxChat import OWNER, AnanyaxChat
 
 
 def is_admins(func: Callable) -> Callable:
-    async def non_admin(c: ShrutiCHATBOT, m: Message):
+    async def non_admin(c: AnanyaxChat, m: Message):
         if m.from_user.id == OWNER:
             return await func(c, m)
 
